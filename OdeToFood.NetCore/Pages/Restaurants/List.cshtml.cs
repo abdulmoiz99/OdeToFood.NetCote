ed66c8dc1f7ffcc5f3b63425e4ad865cca63e50e
moiz <moiza0799@gmail.com>
2020-08-31 12:35:14 +0500

    Registering  a data service

---
+++ OdeToFood.NetCore/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+
+namespace OdeToFood.NetCore.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public ListModel(IConfiguration config,
+                         IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
